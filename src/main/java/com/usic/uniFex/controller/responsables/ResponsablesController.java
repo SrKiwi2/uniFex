fa41e79b1015e897b6aa573cc8c0ec5b1dd57124
jhowillneytan <jhowillneytan@gmail.com>
2025-09-08 13:48:33 -0400

    add

---
+++ src/main/java/com/usic/uniFex/controller/responsables/ResponsablesController.java
@@ -0,0 +1,32 @@
+package com.usic.uniFex.controller.responsables;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.usic.uniFex.anotacion.ValidarUsuarioAutenticado;
+import com.usic.uniFex.model.IService.IResponsableService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/administracion/responsables")
+public class ResponsablesController {
+
+    private final IResponsableService responsableService;
+    
+    @ValidarUsuarioAutenticado
+    @GetMapping("/vista")
+    public String vista_responsables() {
+        return "responsables/vista";
+    }
+
+    @RequestMapping(value = "/tabla-registros", method = {RequestMethod.GET, RequestMethod.POST})
+    public String tabla_responsbales(Model model) {
+        model.addAttribute("responsables", responsableService.listarParaTabla());
+        return "responsables/tabla_registro";
+    }
+}
