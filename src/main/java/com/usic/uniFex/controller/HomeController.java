45cf98787abb862adf12869ed002bd4063038292
jhowillneytan <jhowillneytan@gmail.com>
2025-09-09 18:53:10 -0400

    lista_mapa

--- src/main/java/com/usic/uniFex/controller/HomeController.java
+++ src/main/java/com/usic/uniFex/controller/HomeController.java
@@ -3,13 +3,11 @@ package com.usic.uniFex.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.usic.uniFex.model.IService.ICategoriaService;
 import com.usic.uniFex.model.IService.IPuestoService;
 import com.usic.uniFex.model.IService.ITipoEntidadService;
+import com.usic.uniFex.model.repository.FuncionesInscripcion;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -21,13 +19,16 @@ public class HomeController {
     private final ITipoEntidadService tipoEntidadService;
     private final ICategoriaService categoriaService;
     private final IPuestoService puestoService;
+    private final FuncionesInscripcion funcionesInscripcion;
 
     @GetMapping("/")
     public String home(HttpServletRequest request, Model model) {
         model.addAttribute("tiposEntidads", tipoEntidadService.findAll());
         model.addAttribute("categorias", categoriaService.findAll());
         model.addAttribute("puestos", puestoService.findAll());
+        model.addAttribute("puestos_libres", funcionesInscripcion.fn_lista_puestos());
         System.out.println("vista inicial");
+        model.addAttribute("mapaPdf", "mapa.pdf");
         return "publico/index"; // templates/publico/index.html
     }
 }
