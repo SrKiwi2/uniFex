6423da1e9f64fe7c188a12bedf10ee7ac9d5de35
jhowillneytan <jhowillneytan@gmail.com>
2025-09-09 20:19:59 -0400

    cinfugud

--- src/main/java/com/usic/uniFex/Config/SecurityConfig.java
+++ src/main/java/com/usic/uniFex/Config/SecurityConfig.java
@@ -20,38 +20,64 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    // @Bean
+    // public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    //     http
+    //             .authorizeHttpRequests(auth -> auth
+    //                     .requestMatchers("/",
+    //                             "/vistaR/**",
+    //                             "/admin/**", // <-- ojo: esto hoy queda público
+    //                             "/iniciar-sesion/**",
+    //                             "/ver/**",
+    //                             "/api/**",
+    //                             "/guardar/**",
+    //                             "/actualizar/**",
+    //                             "/files/**", // <-- tus PDFs
+    //                             "/cerrar_sesion",
+    //                             "/administracion/**",
+    //                             "/vistaGenerarCredenciales/**",
+    //                             "/assets/**")
+    //                     .permitAll()
+    //                     .anyRequest().authenticated())
+    //             .formLogin(login -> login
+    //                     .loginPage("/") // tu index con el modal
+    //                     .loginProcessingUrl("/iniciar-sesion")// <-- AQUÍ: procesa el POST del modal
+    //                     .usernameParameter("usuario") // <-- nombres de tus inputs
+    //                     .passwordParameter("contrasena")
+    //                     .defaultSuccessUrl("/", true) // vuelve al index tras loguear
+    //                     .failureUrl("/?error") // puedes hacer que abra el modal si hay error
+    //                     .permitAll())
+    //             .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()))
+    //             .csrf(csrf -> csrf.disable());
+
+    //     return http.build();
+    // }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-        .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/",
-                "/vistaR/**",
-                "/admin/**",            // <-- ojo: esto hoy queda público
-                "/iniciar-sesion/**",
-                "/ver/**",
-                "/api/**",
-                "/guardar/**",
-                "/actualizar/**",
-                "/files/**",            // <-- tus PDFs
-                "/cerrar_sesion",
-                "/administracion/**",
-                "/assets/**",
-            "/vistaGenerarCredenciales/**")
-            .permitAll()
-            .anyRequest().authenticated()
-        )
-        .formLogin(login -> login
-            .loginPage("/")                       // tu index con el modal
-            .loginProcessingUrl("/iniciar-sesion")// <-- AQUÍ: procesa el POST del modal
-            .usernameParameter("usuario")         // <-- nombres de tus inputs
-            .passwordParameter("contrasena")
-            .defaultSuccessUrl("/", true)         // vuelve al index tras loguear
-            .failureUrl("/?error")                // puedes hacer que abra el modal si hay error
-            .permitAll()
-        )
-        .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()))
-        .csrf(csrf -> csrf.disable());
-
+        http.authorizeHttpRequests(
+                auth -> auth.requestMatchers(
+                        "/",
+                        "/vistaR/**",
+                        "/admin/**",
+                        "/iniciar-sesion/**",
+                        "/ver/**",
+                        "/api/**",
+                        "/guardar/**",
+                        "/actualizar/**",
+                        "/files/**",
+                        "/cerrar_sesion",
+                        "/administracion/**",
+                        "/vistaGenerarCredenciales/**",
+                        "/assets/**").permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .formLogin(login -> login
+                        .loginPage("/")
+                        .permitAll())
+                .headers(headers -> headers
+                        .frameOptions(frame -> frame.sameOrigin()))
+                .csrf(csrf -> csrf.disable());
         return http.build();
     }
 
